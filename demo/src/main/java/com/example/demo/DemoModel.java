0f7fcd4c61e6f9554cc87eab2b39984694a6dfb9
networkSocerer <gigattfyhig@gmail.com>
2024-08-30 13:30:27 +0900

    aws

---
+++ demo/src/main/java/com/example/demo/DemoModel.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import io.micrometer.common.lang.NonNull;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+
+@Builder
+@RequiredArgsConstructor
+public class DemoModel {
+
+	@NonNull
+	private String id;
+}
