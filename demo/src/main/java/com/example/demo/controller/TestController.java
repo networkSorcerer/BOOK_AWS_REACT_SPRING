24e3c0af9b8fed21553cb7b004c32f783b2fffeb
networkSocerer <gigattfyhig@gmail.com>
2024-09-02 19:16:04 +0900

    aws

--- demo/src/main/java/com/example/demo/controller/TestController.java
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.TestRequestBodyDTO;
 
 @RestController
@@ -37,4 +41,11 @@ public class TestController {
     public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
     	return "Hello World! ID" + testRequestBodyDTO.getId() + "Message : " + testRequestBodyDTO.getMessage();
      }
+    
+    @GetMapping("/testResponseBody")
+    public ResponseDTO<String> testControllerResponseBody() {
+    	List<String> list = new ArrayList<>();
+    	ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+    	return response;
+    }
 }
