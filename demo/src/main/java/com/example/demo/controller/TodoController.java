b7c56c354de4d288ea7d176b036a5e5c3044e6c9
networkSocerer <gigattfyhig@gmail.com>
2024-09-07 00:50:04 +0900

    aws

--- demo/src/main/java/com/example/demo/controller/TodoController.java
+++ demo/src/main/java/com/example/demo/controller/TodoController.java
@@ -6,10 +6,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TodoDTO;
 import com.example.demo.service.TodoService;
 
 @RestController
@@ -19,6 +22,13 @@ public class TodoController {
 	@Autowired
 	private TodoService service;
 	
+	
+	@PostMapping
+	public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto) {
+		try {
+			
+		}
+	}
 	@GetMapping("/test")
 	public ResponseEntity<?> testTodo(){
 		String str = service.testService();
