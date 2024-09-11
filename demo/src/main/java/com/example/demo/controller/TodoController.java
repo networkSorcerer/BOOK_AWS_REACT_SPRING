e387e9a23a08697c9619d6437c68c7194ced7453
networkSocerer <gigattfyhig@gmail.com>
2024-09-11 23:10:07 +0900

    aws

--- demo/src/main/java/com/example/demo/controller/TodoController.java
+++ demo/src/main/java/com/example/demo/controller/TodoController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -71,4 +72,44 @@ public class TodoController {
 		return ResponseEntity.ok().body(response);
 	}
 
+	@GetMapping
+	public ResponseEntity<?> retrieveTodoList() {
+		String temporaryUserId = "temporary-user";
+		
+		//(1) 서비스 메서드의 retrieve 메서드를 사용해 Todo 리스트를 가져온다
+		List<TodoEntity>entities = service.retrieve(temporaryUserId);
+		
+		//(2) 자바 스트림을 이용해 리턴된 엔티티 리스트를 TodoDTO 리스트로 변환한다.
+		List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+		
+		//(6) 변환된 TodoDTO 리스트를 이용해 ResponseDTO를 초기화 한다
+		ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+		
+		//ResponseDTO를 리턴한다
+		return ResponseEntity.ok().body(response);
+		
+	}
+	
+	@PutMapping
+	public ResponseEntity<?> updateTodo(@RequestBody TodoDTO dto) {
+		String temporaryUserId = "temporary-user"; // temporary user id
+		
+		//(1) dto를 entity로 변환한다
+		TodoEntity entity = TodoDTO.toEntity(dto);
+		
+		// (2) id 를 temporaryUserId로 초기화한다. 여기는 4장 인증과 인가에서 수정할 예정이다
+		entity.setUserId(temporaryUserId);
+		
+		// (3) 서비스를 이용해 entity를 업데이트한다
+		List<TodoEntity> entities = service.update(entity);
+		
+		// (4) 자바 스트림을 이용해 리턴된 엔티티 리스트를 TodoDTO 리스트로 변환한다
+		List<TodoDTO>dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+		
+		// (5) 변환된 TodoDTO리스트를 이용해 ResponseDTO를 초기화한다
+		ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+		
+		// (6) ResponseDTO를 리턴한다
+		return ResponseEntity.ok().body(response);
+	}
 }
--- demo/src/main/java/com/example/demo/service/TodoService.java
+++ demo/src/main/java/com/example/demo/service/TodoService.java
@@ -1,12 +1,14 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
+
 
 import lombok.extern.slf4j.Slf4j;
 @Slf4j
@@ -48,5 +50,29 @@ public class TodoService {
 	        throw new RuntimeException("Unknown user.");  // 'trow'를 'throw'로 수정
 	    }
 	}
+	
+	 public List<TodoEntity> retrieve(final String userId) {
+	        return repository.findByUserId(userId);  // ')'를 삭제하여 오류 수정
+	    }
+	 
+	 public List<TodoEntity>update(final TodoEntity entity) {
+		 //(1) 저장할 엔티티가 유효한지 확인한다. 이 메서드는 2.3.1 Create Todo에서 구현했다.
+		 validate(entity);
+		 
+		 //(2) 넘겨받은 엔티티 id 를 이용해 TodoEntity를 가져온다. 존재하지 않는 엔티티는 업데이트 할 수 없기 떄문이다.
+		 final Optional<TodoEntity>original = repository.findById(entity.getId());
+		 
+		 original.ifPresent(todo -> {
+			 // (3) 반환된 TodoEntity가 존재하면 값을 새 entity의 값으로 덮어 씌운다
+			 todo.setTitle(entity.getTitle());
+			 todo.setDone(entity.isDone());
+			 
+			 //(4) 데이터베이스에 새 값을 저장한다
+			 repository.save(todo);
+		 });
+		 
+		 //2.3.2 Retrieve Todo에서 만든 메서드를 이용해 유저의 모든 Todo 리스트를 리턴한다
+		 return retrieve(entity.getUserId());
+	 }
 
 }
