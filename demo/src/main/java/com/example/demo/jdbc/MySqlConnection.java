70a4659c5356e2ab90ac9e24fce4016cf56eae0e
networkSorcerer <gigattfyhig@gmail.com>
2024-11-26 11:30:47 +0900

    h2 원복

--- demo/src/main/java/com/example/demo/jdbc/MySqlConnection.java
+++ demo/src/main/java/com/example/demo/jdbc/MySqlConnection.java
@@ -5,14 +5,21 @@ import java.sql.DriverManager;
 
 public class MySqlConnection {
 	  public static void main(String[] args) {
-	        String jdbcUrl = "jdbc:mysql://localhost:3306/Board";
+		  	String jdbcUrl = "jdbc:mysql://yo-mysql.cn628cgo0r2b.ap-northeast-2.rds.amazonaws.com:3306/Board?serverTimezone=UTC&characterEncoding=UTF-8";
 	        String username = "admin";
 	        String password = "agape99^^";
 
-	        try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password)) {
-	            System.out.println("MySQL 연결 성공!");
+	        try {
+	            // MySQL JDBC 드라이버를 명시적으로 로드
+	            Class.forName("com.mysql.cj.jdbc.Driver");
+
+	            // 데이터베이스 연결
+	            try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password)) {
+	                System.out.println("MySQL 연결 성공!");
+	            }
 	        } catch (Exception e) {
 	            e.printStackTrace();
 	        }
+
 	    }
 }
